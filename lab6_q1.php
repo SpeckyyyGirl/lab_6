fd65aed7eea3b3b5d062ff561b3cd6a28436894c
NATASHA BINTI KHAIRULNIZAM <ntsh.nzm20@gmail.com>
2024-05-19 12:45:39 +0800

    initialize

--- lab6_q1.php
+++ lab6_q1.php
@@ -4,15 +4,40 @@
  <title>Lab 6 Q1</title>
 </head>
 <body>
- <?php
- $name = "Muhammad Asyraf";
- ?>
- <table>
- <tr>
- <td>Name</td>
- <td><?php echo "$name"; ?></td>
- </tr>
- </table>
+    <?php
+        $name = "NATASHA BINTI KHAIRULNIZAM";
+        $matric_number = "DI220010";
+        $course = "BIT";
+        $year_of_study = "Year 2 Semester 1";
+        $address = "9, Jalan Orkid 16, Taman Orkid, 86000 Kluang, Johor"
+    ?>
+
+    <table>
+    <tr>
+        <td>Name</td>
+        <td><?php echo "$name"; ?></td>
+    </tr>
+
+    <tr>
+        <td>Matric Number</td>
+        <td><?php echo "$matric_number"; ?></td>
+    </tr>
+
+    <tr>
+        <td>Course</td>
+        <td><?php echo "$course"; ?></td>
+    </tr>
+
+    <tr>
+        <td>Year of Study</td>
+     <td><?php echo "$year_of_study"; ?></td>
+    </tr>
+
+    <tr>
+        <td>Address</td>
+        <td><?php echo "$address"; ?></td>
+    </tr>
+    </table>
 
 </body>
 </html>
